9dc0698ffe6b511b54bdcecbafcdb1409c18f857
Teuku Ferdian <128288215+teukuferdian@users.noreply.github.com>
2025-10-08 01:26:27 +0700

    Add files via upload

---
+++ app_pelatihan/db_pelatihan (1).sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jul 2025 pada 13.23
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pelatihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_dosen`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_dosen: #1932 - Table &#039;db_pelatihan.tbl_dosen&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_dosen: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_dosen`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jadwal`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_jadwal: #1932 - Table &#039;db_pelatihan.tbl_jadwal&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_jadwal: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_jadwal`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_krs`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_krs: #1932 - Table &#039;db_pelatihan.tbl_krs&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_krs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_krs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_mahasiswa`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_mahasiswa: #1932 - Table &#039;db_pelatihan.tbl_mahasiswa&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_mahasiswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_mahasiswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_matkul`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_matkul: #1932 - Table &#039;db_pelatihan.tbl_matkul&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_matkul: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_matkul`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_semester`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_semester: #1932 - Table &#039;db_pelatihan.tbl_semester&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_semester: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_semester`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+-- Kesalahan membaca struktur untuk tabel db_pelatihan.tbl_user: #1932 - Table &#039;db_pelatihan.tbl_user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_pelatihan.tbl_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_pelatihan`.`tbl_user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
